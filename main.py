b1e7a7373f5cb0ad8ac7b5765b0aa3920e40e013
RummySaltyFish <72684064+RummySaltyFish@users.noreply.github.com>
2020-12-15 20:45:20 +0800

    1
    1

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/15 18:57
+# @Author  : RummySaltyfish
+# @File    : main.py
+# @Software: PyCharm
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import mainwindowtest
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = mainwindowtest.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
