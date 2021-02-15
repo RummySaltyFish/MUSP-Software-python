a2f6b5a1d1e89a99376c74239e1408884d16be9b
RummySaltyFish <72684064+RummySaltyFish@users.noreply.github.com>
2021-02-15 14:13:32 +0800

    comport choosing is now available
    comport choosing is now available

--- communicationFunc.py
+++ communicationFunc.py
@@ -12,13 +12,6 @@ import digi
 
 # 通讯及控制类
 class Communication(object):
-
-	def broadcastConfirm(self):
-		Raw802Device = digi.xbee.devices.Raw802Device("COM3", 115200)
-		Raw802Device.open()
-		Raw802Device.send_data_broadcast("u")#帧别码广播，0x75，十进制117
-		Raw802Device.close()
-
 	def terminalSignal(self):
 		pass
 
@@ -31,6 +24,4 @@ class Communication(object):
 	def refreshModule(self):
 		pass
 
-Communication()
-Communication.broadcastConfirm(self=None)
 
--- main.py
+++ main.py
@@ -7,19 +7,28 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import serial.tools.list_ports
 from mainwindowtest import Ui_MainWindow
+from digi.xbee.devices import Raw802Device, RemoteXBeeDevice
+from digi.xbee.models.address import XBee64BitAddress
+import digi
 
 
+class MainWindow(QMainWindow):
+	def __init__(self, parent=None):
+		super(MainWindow, self).__init__(parent)
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+
+	def broadcastConfirm(self):
+		comport = self.ui.comportchoose.currentText()
+		Raw802Device = digi.xbee.devices.Raw802Device(comport, 115200)
+		Raw802Device.open()
+		Raw802Device.send_data_broadcast("u")  # 帧别码广播，0x75，十进制117
+		Raw802Device.close()
 
 
-
-
-# 运行程序pip install pyserial
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	MainWindow = QMainWindow()
-	ui = Ui_MainWindow()
-	ui.setupUi(MainWindow)
-	MainWindow.show()
+	win=MainWindow()
+	win.show()
 	sys.exit(app.exec_())
